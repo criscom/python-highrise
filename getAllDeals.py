6ec1c4b5be667135b732606ff6bcf35e67c117bd
Christoph Potzinger <criscom27@gmail.com>
2017-07-06 16:44:06 +0200

    fix getAllDeals: function doesnt work

--- getAllDeals.py
+++ getAllDeals.py
@@ -1,4 +1,13 @@
+from pyrise import *
+
 def getAllDeals():
   deals = Deal.all()
   for deal in deals:
     print ('deal name = ') + deal.name + (' ') + ('deal id = ') + str(deal.id) + (' ') + ('deal category id = ') + str(deal.category_id)
+
+Highrise.set_server('sunlime')
+Highrise.auth('4572ff2d6932dabc5e4a9e905b60ae45')
+#getAllDeals
+deals = Deal.all()
+for deal in deals:
+  print ('deal name = ') + deal.name + (' ') + ('deal id = ') + str(deal.id) + (' ') + ('deal category id = ') + str(deal.category_id)
