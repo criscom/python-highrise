918941cebd660b678dda51adfd72adf32232c577
criscom <criscom27@gmail.com>
2017-07-10 15:35:31 +0200

    Change print layout

--- getAllDeals.py
+++ getAllDeals.py
@@ -7,7 +7,8 @@ def getAllDeals():
 
 Highrise.set_server('sunlime')
 Highrise.auth('4572ff2d6932dabc5e4a9e905b60ae45')
-#getAllDeals
 deals = Deal.all()
+print '\n' + ('Getting all Deals for you ...') + '\n'
+print('This is your list of deals:') + '\n' + '==================================================='
 for deal in deals:
-  print ('deal name = ') + deal.name + (' ') + ('deal id = ') + str(deal.id) + (' ') + ('deal category id = ') + str(deal.category_id)
+  print ('deal id = ') + str(deal.id) + '\n' + ('deal name = ') + deal.name + (' // ') + ('deal category id = ') + str(deal.category_id) + (' // ') + ('deal status = ') + str(deal.status) + (' // ') + '\n' + '==================================================='
